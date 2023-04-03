0a429887da2beb63a888f23797fbd8487dafec1d
psychotlc <narench@inbox.ru>
2023-04-03 11:57:39 +0300

    added workflow.yaml

---
+++ build/CMakeFiles/equation.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/narek/Документы/timp/timp-lab04/lab03/solver_application/equation.cpp" "CMakeFiles/equation.dir/solver_application/equation.cpp.o" "gcc" "CMakeFiles/equation.dir/solver_application/equation.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/narek/Документы/timp/timp-lab04/lab03/build/solver_lib/CMakeFiles/solver.dir/DependInfo.cmake"
+  "/home/narek/Документы/timp/timp-lab04/lab03/build/formatter_ex_lib/CMakeFiles/formatter_ex.dir/DependInfo.cmake"
+  "/home/narek/Документы/timp/timp-lab04/lab03/build/formatter_lib/CMakeFiles/formatter.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
